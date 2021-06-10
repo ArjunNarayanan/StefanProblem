d51d140f4df02c1576eb4fe7e5aa90b9a45b41e4
Arjun <arjun.narayanan91@gmail.com>
2021-06-10 11:34:51 -0700

    LDG converges. Working on cylindrical problem

--- StefanDG/LocalDG/test_LDG.jl
+++ StefanDG/LocalDG/test_LDG.jl
@@ -18,82 +18,174 @@ end
 
 function source_term(v, k)
     x, y = v
-    return -8pi^2 * k * cos(2pi * x) * sin(2pi * y)
+    return 8pi^2 * k * cos(2pi * x) * sin(2pi * y)
 end
 
-solverorder = 1
-levelsetorder = 1
-nelmts = 17
-penaltyfactor = 1.0
-beta = [1.0, 1.0]
-k1 = k2 = 1.0
-
-numqp = required_quadrature_order(solverorder)
-solverbasis = LagrangeTensorProductBasis(2, solverorder)
-levelsetbasis = LagrangeTensorProductBasis(2, levelsetorder)
-
-dgmesh = CutCellDG.DGMesh(
-    [0.0, 0.0],
-    [1.0, 1.0],
-    [nelmts, nelmts],
-    interpolation_points(solverbasis),
-)
-cgmesh = CutCellDG.CGMesh(
-    [0.0, 0.0],
-    [1.0, 1.0],
-    [nelmts, nelmts],
-    number_of_basis_functions(levelsetbasis),
-)
-levelset = CutCellDG.LevelSet(
-    x -> plane_distance_function(x, [1.0, 0.0], [0.5, 0.0]),
-    cgmesh,
-    levelsetbasis,
-)
-minelmtsize = minimum(CutCellDG.element_size(dgmesh))
-
-cutmesh = CutCellDG.CutMesh(dgmesh, levelset)
-cellquads = CutCellDG.CellQuadratures(cutmesh, levelset, numqp)
-facequads = CutCellDG.FaceQuadratures(cutmesh, levelset, numqp)
-interfacequads = CutCellDG.InterfaceQuadratures(cutmesh, levelset, numqp)
-
-mergedmesh =
-    CutCellDG.MergedMesh!(cutmesh, cellquads, facequads, interfacequads)
-
-sysmatrix = CutCellDG.SystemMatrix()
-sysrhs = CutCellDG.SystemRHS()
-
-LocalDG.assemble_LDG_linear_system!(
-    sysmatrix,
-    solverbasis,
-    cellquads,
-    facequads,
-    interfacequads,
+function measure_error(
+    nelmts,
+    solverorder,
+    levelsetorder,
+    distancefunction,
+    sourceterm,
+    exactsolution,
+    exactgradient,
     k1,
     k2,
     penaltyfactor,
     beta,
-    mergedmesh,
 )
-LocalDG.assemble_LDG_rhs!(
-    sysrhs,
-    x -> -source_term(x, k1),
-    exact_solution,
-    solverbasis,
-    cellquads,
-    facequads,
-    penaltyfactor,
-    mergedmesh,
-)
+    numqp = required_quadrature_order(solverorder)
+    solverbasis = LagrangeTensorProductBasis(2, solverorder)
+    levelsetbasis = LagrangeTensorProductBasis(2, levelsetorder)
 
-matrix = CutCellDG.sparse_operator(sysmatrix, mergedmesh, 3)
-rhs = CutCellDG.rhs_vector(sysrhs, mergedmesh, 3)
+    dgmesh = CutCellDG.DGMesh(
+        [0.0, 0.0],
+        [1.0, 1.0],
+        [nelmts, nelmts],
+        interpolation_points(solverbasis),
+    )
+    cgmesh = CutCellDG.CGMesh(
+        [0.0, 0.0],
+        [1.0, 1.0],
+        [nelmts, nelmts],
+        number_of_basis_functions(levelsetbasis),
+    )
+    levelset = CutCellDG.LevelSet(x -> ones(size(x)[2]), cgmesh, levelsetbasis)
+    minelmtsize = minimum(CutCellDG.element_size(dgmesh))
 
-sol = reshape(matrix \ rhs, 3, :)
-T = sol[1, :]'
-G = sol[2:3, :]
+    penalty = penaltyfactor
+    # penalty = penaltyfactor / minelmtsize
 
-errT = mesh_L2_error(T, exact_solution, solverbasis, cellquads, mergedmesh)
-errG = mesh_L2_error(G, exact_gradient, solverbasis, cellquads, mergedmesh)
+    cutmesh = CutCellDG.CutMesh(dgmesh, levelset)
+    cellquads = CutCellDG.CellQuadratures(cutmesh, levelset, numqp)
+    facequads = CutCellDG.FaceQuadratures(cutmesh, levelset, numqp)
+    interfacequads = CutCellDG.InterfaceQuadratures(cutmesh, levelset, numqp)
 
-println("Error T = $errT")
-println("Error G = $errG")
+    mergedmesh =
+        CutCellDG.MergedMesh!(cutmesh, cellquads, facequads, interfacequads)
+
+
+    sysmatrix = CutCellDG.SystemMatrix()
+    sysrhs = CutCellDG.SystemRHS()
+
+    LocalDG.assemble_LDG_linear_system!(
+        sysmatrix,
+        solverbasis,
+        cellquads,
+        facequads,
+        interfacequads,
+        k1,
+        k2,
+        penalty,
+        beta,
+        mergedmesh,
+    )
+
+    LocalDG.assemble_LDG_rhs!(
+        sysrhs,
+        sourceterm,
+        exactsolution,
+        solverbasis,
+        cellquads,
+        facequads,
+        penalty,
+        mergedmesh,
+    )
+    ################################################################################
+    matrix = CutCellDG.sparse_operator(sysmatrix, mergedmesh, 3)
+    rhs = CutCellDG.rhs_vector(sysrhs, mergedmesh, 3)
+    sol = reshape(matrix \ rhs, 3, :)
+    ################################################################################
+
+    ################################################################################
+    T = sol[1, :]'
+    G = sol[2:3, :]
+
+    errT = mesh_L2_error(T, exactsolution, solverbasis, cellquads, mergedmesh)
+    errG = mesh_L2_error(G, exactgradient, solverbasis, cellquads, mergedmesh)
+    ################################################################################
+
+    Tnorm = integral_norm_on_mesh(exactsolution, cellquads, mergedmesh, 1)
+    Gnorm = integral_norm_on_mesh(exactgradient, cellquads, mergedmesh, 2)
+
+    return errT[1] / Tnorm[1], errG ./ Gnorm
+end
+
+
+
+
+################################################################################
+powers = [2, 3, 4, 5]
+nelmts = 2 .^ powers .+ 1
+solverorder = 1
+levelsetorder = 1
+k1 = k2 = 1.0
+penaltyfactor = 1.0
+beta = 0.5 * [1.0, 1.0]
+distancefunction(x) = plane_distance_function(x, [1.0, 0.0], [0.5, 0.0])
+
+err = [
+    measure_error(
+        ne,
+        solverorder,
+        levelsetorder,
+        distancefunction,
+        x -> source_term(x, k1),
+        exact_solution,
+        exact_gradient,
+        k1,
+        k2,
+        penaltyfactor,
+        beta,
+    ) for ne in nelmts
+]
+
+errT = [er[1] for er in err]
+errG1 = [er[2][1] for er in err]
+errG2 = [er[2][2] for er in err]
+
+dx = 1.0 ./ nelmts
+
+Trate1 = convergence_rate(dx, errT)
+G1rate1 = convergence_rate(dx, errG1)
+G2rate1 = convergence_rate(dx, errG2)
+################################################################################
+
+
+
+################################################################################
+powers = [2, 3, 4, 5]
+nelmts = 2 .^ powers .+ 1
+solverorder = 2
+levelsetorder = 1
+k1 = k2 = 1.0
+penaltyfactor = 1.0
+beta = 0.5 * [1.0, 1.0]
+distancefunction(x) = plane_distance_function(x, [1.0, 0.0], [0.5, 0.0])
+
+err = [
+    measure_error(
+        ne,
+        solverorder,
+        levelsetorder,
+        distancefunction,
+        x -> source_term(x, k1),
+        exact_solution,
+        exact_gradient,
+        k1,
+        k2,
+        penaltyfactor,
+        beta,
+    ) for ne in nelmts
+]
+
+errT = [er[1] for er in err]
+errG1 = [er[2][1] for er in err]
+errG2 = [er[2][2] for er in err]
+
+dx = 1.0 ./ nelmts
+
+Trate2 = convergence_rate(dx, errT)
+G1rate2 = convergence_rate(dx, errG1)
+G2rate2 = convergence_rate(dx, errG2)
+################################################################################
